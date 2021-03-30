1386783ea3dec85d0fc88fcea82c48aa93af1dd8
Sean-Sanii-Nejad <74361745+Sean-Sanii-Nejad@users.noreply.github.com>
2021-03-30 21:55:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,10 +10,6 @@ public class Main3 {
         return a;
     }
 
-    public static String ceasarString(String word){
-        return "";
-    }
-
     public static double absolute(double number) {
         if (number > 0) {
             return number;
@@ -59,7 +55,7 @@ public class Main3 {
         File file = new File("cexercise3.txt");
         Scanner scanner = new Scanner(file);
         String content = scanner.next();
-        String key = key();
+        String key = keyExercise3();
         String word = "";
         char[] keyArray = new char[6];
 
@@ -73,7 +69,25 @@ public class Main3 {
         System.out.println(word);
     }
 
-    public static String key() throws FileNotFoundException {
+    public static void question4() throws FileNotFoundException {
+        File file = new File("cexercise4.txt");
+        Scanner scanner = new Scanner(file);
+        String content = scanner.next();
+        String key = keyExercise4();
+        String word = "";
+        char[] keyArray = new char[6];
+
+        for (int x = 0; x < 6; x++) {
+            keyArray[x] = (char) (key.charAt(x) - 'A');
+        }
+
+        for (int i = 0; i < 30; i++) {
+            word += ceasarChar(content.charAt(i), keyArray[i % keyArray.length]);
+        }
+        System.out.println(word);
+    }
+
+    public static String keyExercise3() throws FileNotFoundException {
         String key0 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise3(0))+65));
         String key1 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise3(1))+65));
         String key2 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise3(2))+65));
@@ -82,6 +96,110 @@ public class Main3 {
         String key5 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise3(5))+65));
 
         return key0 + key1 + key2 + key3 + key4 + key5;
+    }
+
+    public static String keyExercise4() throws FileNotFoundException {
+        String key0 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(0))+65));
+        String key1 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(1))+65));
+        String key2 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(2))+65));
+        String key3 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(3))+65));
+        String key4 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(4))+65));
+        String key5 = Character.toString((char) (frequencyCompare(frequencyTess26(), frequencyExercise4(5))+65));
+
+        return key0 + key1 + key2 + key3 + key4 + key5;
+    }
+
+    public static void columnExercise5() throws FileNotFoundException {
+        File file = new File("cexercise5.txt");
+        Scanner scanner = new Scanner(file);
+        String content = scanner.next();
+
+        String[] column = new String[5];
+
+        column[0] = content.substring(0, 168);
+        column[1] = content.substring(168, 336);
+        column[2] = content.substring(336, 504);
+        column[3] = content.substring(504, 672);
+        column[4] = content.substring(672, 840);
+
+        System.out.println(column[0]);
+        System.out.println(column[1]);
+        System.out.println(column[2]);
+        System.out.println(column[3]);
+        System.out.println(column[4]);
+    }
+
+    public static void columnExercise6() throws FileNotFoundException {
+        File file = new File("cexercise6.txt");
+        Scanner scanner = new Scanner(file);
+        String content = scanner.next();
+
+        String[] column = new String[6];
+
+        column[0] = content.substring(0, 140);
+        column[1] = content.substring(140, 280);
+        column[2] = content.substring(280, 420);
+        column[3] = content.substring(420, 560);
+        column[4] = content.substring(560, 700);
+        column[5] = content.substring(700, 840);
+
+        System.out.println(column[5]);
+        System.out.println(column[4]);
+        System.out.println(column[0]);
+        System.out.println(column[3]);
+        System.out.println(column[2]);
+        System.out.println(column[1]);
+    }
+
+    public static void question7() throws FileNotFoundException {
+        File file = new File("cexercise7.txt");
+        Scanner scanner = new Scanner(file);
+        String content = scanner.nextLine();
+        content = swap(content, 'A','i');
+        content = swap(content, 'B','k');
+        content = swap(content, 'C','a');
+        content = swap(content, 'D','j');
+        content = swap(content, 'E','d');
+        content = swap(content, 'F','b');
+        content = swap(content, 'G','n');
+        content = swap(content, 'H','p');
+        content = swap(content, 'I','t');
+        content = swap(content, 'J','g');
+        content = swap(content, 'K','s');
+        content = swap(content, 'L','r');
+        content = swap(content, 'M','m');
+        content = swap(content, 'N','y');
+        content = swap(content, 'O','v');
+        content = swap(content, 'P','l');
+        content = swap(content, 'Q','u');
+        content = swap(content, 'R','q');
+        content = swap(content, 'S','|');
+        content = swap(content, 'S','h');
+        content = swap(content, 'T','e');
+        content = swap(content, 'V','o');
+        content = swap(content, 'W','f');
+        content = swap(content, 'X','z');
+        content = swap(content, 'Y','w');
+        content = swap(content, 'Z','c');
+
+        System.out.println(content);
+    }
+
+    public static String swap(String cipher, char a, char b){
+        String newString = "";
+
+        for(int i = 0; i < cipher.length(); i++){
+            if(cipher.charAt(i) == a){
+                newString += b;
+            }
+            else if(cipher.charAt(i) == b){
+                newString += a;
+            }
+            else{
+                newString += cipher.charAt(i);
+            }
+        }
+        return newString;
     }
 
     public static int frequencyCompare(HashMap hashmap, HashMap hashmap2){
@@ -103,8 +221,6 @@ public class Main3 {
         }
         return lowestIndex;
     }
-
-
 
     public static HashMap frequencyExercise1() throws FileNotFoundException {
         File file = new File("cexercise1.txt");
@@ -160,7 +276,37 @@ public class Main3 {
         for (int i = 0; i < 26; i++) {
             hashMap.put((char) (i + 'A'), hashMap.getOrDefault((char) (i + 'A'), 0.0) / sum * 100);
         }
-//        System.out.println(hashMap);
+        return hashMap;
+    }
+
+    public static HashMap frequencyExercise4(int characterPosition) throws FileNotFoundException {
+        File file = new File("cexercise4.txt");
+        Scanner scanner = new Scanner(file);
+        HashMap<Character, Double> hashMap = new HashMap();
+        for (int i = 0; i < 26; i++) {
+            hashMap.put((char) (i + 'A'), 0.0);
+        }
+        String content = scanner.nextLine();
+        String letter1 = "";
+        for (int i = 0; i < content.length(); i++) {
+            if (i % 6 == characterPosition) {
+                letter1 += (content.charAt((char) i));
+            }
+        }
+        char letters;
+        for (int i = 0; i < letter1.length(); i++) {
+            letters = letter1.charAt(i);
+            if (hashMap.containsKey(letters)) {
+                hashMap.put(letters, hashMap.getOrDefault(letters, 0.0) + 1);
+            }
+        }
+        double sum = 0;
+        for (double f : hashMap.values()) {
+            sum += f;
+        }
+        for (int i = 0; i < 26; i++) {
+            hashMap.put((char) (i + 'A'), hashMap.getOrDefault((char) (i + 'A'), 0.0) / sum * 100);
+        }
         return hashMap;
     }
 
@@ -186,7 +332,6 @@ public class Main3 {
         for (int i = 0; i < 26; i++) {
             hashMap.put((char) (i + 'A'), hashMap.getOrDefault((char) (i + 'A'), 0.0) / sum * 100);
         }
-//        System.out.println(hashMap);
         return hashMap;
     }
 
@@ -194,5 +339,10 @@ public class Main3 {
         question1();
         question2();
         question3();
+        question4();
+//        columnExercise5();
+//        columnExercise6();
+        question7();
+
     }
 }
